ecfb41b205f0815b05bf25835ab86039ce8e3529
Joaquin Blanco <48386218@est.ort.edu.ar>
2024-06-19 15:49:25 -0300

    Added class activity
    Me re costo una banda y por querer hacerme el piola me salio horrible

--- Assets/ArraySwitcher.cs
+++ Assets/ArraySwitcher.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ArraySwitcher : MonoBehaviour
 {
@@ -11,7 +9,12 @@ public class ArraySwitcher : MonoBehaviour
     void Start()
     {
         index = 0;
-        Instantiate(foods[index],transform.position, Quaternion.identity);
+        for (int i = index; i < foods.Length; i++)
+        { 
+            foods[index].SetActive(false);
+            index++;
+        }
+        index = 0;
     }
 
     // Update is called once per frame
@@ -19,20 +22,28 @@ public class ArraySwitcher : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Debug.Log("Prueba");
- 
+            Debug.Log("Prueba mas");
+            DeasctivateAll();
             index += 1;
-            Instantiate(foods[index], transform.position, Quaternion.identity);
-            
+            foods[index].SetActive(true);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Debug.Log("Prueba");
-   
+            Debug.Log("Prueba menos");
+           
+            DeasctivateAll();
             index -= 1;
-            Instantiate(foods[index], transform.position, Quaternion.identity);
+            foods[index].SetActive(true);
+        }
+    }
 
+    void DeasctivateAll()
+    {
+        for (int i = 0; i < foods.Length; i++)
+        {
+            int deactivateIndex = i;
+            foods[deactivateIndex].SetActive(false);
         }
     }
 
